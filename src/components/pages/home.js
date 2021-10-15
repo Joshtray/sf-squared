0c5037490944438cc1f7a4423062bb52018e5844
Joshtray <jesseyuchenwichi@gmail.com>
2021-10-15 17:03:28 +0900

    Fromatted title page on home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './home.css'
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home">
+            <div className="slideshow">
+                <div className="title">
+                    <div className="title-content">
+                        <p>Mission: Healthy Corner Store</p>
+                        <span>
+                            <p>Be the change in the Mission District</p>
+                            <span></span>
+                            <Link to="/how">LEARN HOW {">"}</Link>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div className="description">
+
+            </div>
         </div>
     )
 }
